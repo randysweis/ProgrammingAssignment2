832934dfab1858084b2a73fa2561a37b9999650d
Randy Sweis <rsweis@uchicago.edu>
2014-08-20 15:11:16 -0500

    Preliminary function
    Preliminary function to get matrix and cache solved matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## The first function generates a matrix and 
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -5,11 +6,30 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setsolve<-function(solve) m<<- solve
+        getsolve<-function() m
+        list(set=set, get=get,
+             setsolve=setsolve,
+             getsolve=getsolve)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getsolve()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        matrix<-x$get
+        m<-solve(matrix, ...)
+        x$setsolve(m)
+        m
 }
